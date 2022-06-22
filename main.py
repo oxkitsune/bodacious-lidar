1c5871afe15bc20b8bec2ef7a2b9e989e95a0496
sklarp <robinreitsma1@gmail.com>
2022-06-22 18:02:20 +0200

    progress

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 from loader import Pointcloud
 import visualize
+from render import render_magic
 
 name = "0104Amstel"
 
-cloud = Pointcloud(verbose=True)
-(cloud
-    .load_from_e57(f"{name}.e57")
-    .downscale(0.05)
-    .denoise()
-    .filter_main_cluster()
-    .save_to_ply(f"{name}.ply"))
-
-planes, inlier_points = cloud.find_planes(min_inliers=1000, inlier_distance_threshold=0.10, ransac_n=3, save=True, save_path=name)
-
-# load pointcloud from already processed .ply
 # cloud = Pointcloud(verbose=True)
-# cloud.load_from_ply(f"{name}.e57")
+# (cloud
+#     .load_from_e57(f"{name}.e57")
+#     .downscale(0.05)
+#     .denoise()
+#     .filter_main_cluster()
+#     .save_to_ply(f"{name}.ply"))
+
+# planes, inlier_points = cloud.find_planes(min_inliers=1000, inlier_distance_threshold=0.10, ransac_n=3, save=True, save_path=name)
+ 
+# load pointcloud from already processed .ply
+cloud = Pointcloud(verbose=True)
+cloud.load_from_ply(f"{name}.e57")
 
 # load ransac results from .npz file
-# import numpy as np
-# data = np.load(f"{name}.npz", allow_pickle=True)
-# inlier_points = data["inlier_points"]
+import numpy as np
+data = np.load(f"{name}.npz", allow_pickle=True)
+inlier_points = data["inlier_points"]
+planes = data["planes"]
 
-visualize.draw_planes_in_one(inlier_points)
+# plane_0 = np.array(planes[0][:3], np.float)
+# print(np.linalg.norm(planes[0][:3], axis=0))
+
+# plane_0 = plane_0 / np.linalg.norm(planes[0][:3])
+
+# print(plane_0)
+
+plane_idx = 0
+render_magic(inlier_points[plane_idx], planes[plane_idx][:3])
+visualize.draw_plane(inlier_points[plane_idx], planes[plane_idx][:3])
--- visualize.py
+++ visualize.py
@@ -13,10 +13,22 @@ def draw_planes(inlier_points, max_amount=10):
         # colors = np.repeat([color], len(inliers[i]), axis=0)
         # pcd.colors = o3d.utility.Vector3dVector(colors)
 
-        pcd = Pointcloud.np_to_o3d(curr_points)
+        pcd = Pointcloud.np_to_o3d(curr_inliers)
 
         o3d.visualization.draw_geometries([pcd])
 
+
+def draw_plane(inliers, normal):
+    pcd = Pointcloud.np_to_o3d(inliers)
+    cloud = Pointcloud(verbose=True)
+    cloud.pcd = pcd
+    (cloud
+        .denoise(std_ratio=1.8)
+        .filter_main_cluster(eps=0.3))
+
+    pcd = cloud.pcd
+
+    o3d.visualization.draw_geometries([pcd], lookat=normal, front=-normal, up=[normal[1], -normal[0], normal[2]], zoom=0.75)
 
 def draw_planes_in_one(inlier_points):
     # Pass xyz to o3d pc and visualize
